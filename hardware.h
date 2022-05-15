4068615960d7bf45d7a52bf5350932bf8b750d96
Renze Nicolai <renze@rnplus.nl>
2022-05-15 02:41:40 +0200

    Initial commit

---
+++ hardware.h
@@ -0,0 +1,117 @@
+#pragma once
+
+#include <sdkconfig.h>
+#include <esp_err.h>
+#include <driver/spi_master.h>
+
+#include "bno055.h"
+#include "ili9341.h"
+#include "ice40.h"
+#include "rp2040.h"
+
+#include "pins.h"
+
+/** \brief Initialize basic board support
+ * 
+ * \details This function installs the GPIO ISR (interrupt service routine) service
+ *          which allows for per-pin GPIO interrupt handlers. Then it initializes the
+ *          I2C and SPI communication busses and the LCD display driver. Returns ESP_OK
+ *          on success and a 
+ *
+ * \retval ESP_OK   The function succesfully executed
+ * \retval ESP_FAIL The function failed, possibly indicating hardware failure
+ * 
+ * Check the esp_err header file from the ESP-IDF for a complete list of error codes
+ * returned by SDK functions.
+ */
+
+esp_err_t bsp_init();
+
+/** \brief Initialize RP2040 driver and check I2C communication with the RP2040 co-processor
+ * 
+ * \details This function initializes the RP2040 driver, which installs the RP2040 interrupt
+ *          handler task. Then it queries the version of the firmware running on the RP2040.
+ *          Reading the firmware version verifies that communication via I2C is funcitonal and
+ *          the firmware version indicates weither or not the RP2040 is in bootloader
+ *          mode. If the RP2040 is in bootloader mode it reports version 0xFF.
+ *
+ * \retval ESP_OK   The function succesfully executed
+ * \retval ESP_FAIL The function failed, possibly indicating hardware failure
+ * 
+ * Check the esp_err header file from the ESP-IDF for a complete list of error codes
+ * returned by SDK functions.
+ */
+
+esp_err_t bsp_rp2040_init();
+
+/** \brief Initialize the ICE40 driver and perform basic sanity checks
+ * 
+ * \details This function initializes the ICE40 driver and checks that the status
+ *          indication (the "done" pin) correctly indicates that the ICE40 is disabled.
+ *
+ * \retval ESP_OK   The function succesfully executed
+ * \retval ESP_FAIL The function failed, possibly indicating hardware failure
+ * 
+ * Check the esp_err header file from the ESP-IDF for a complete list of error codes
+ * returned by SDK functions.
+ */
+
+esp_err_t bsp_ice40_init();
+
+/** \brief Initialize the BNO055 driver and put the sensor into power saving mode
+ * 
+ * \details This function initializes the BNO055 driver and puts the sensor into
+ *          power saving mode.
+ *
+ * \retval ESP_OK   The function succesfully executed
+ * \retval ESP_FAIL The function failed, possibly indicating hardware failure
+ * 
+ * Check the esp_err header file from the ESP-IDF for a complete list of error codes
+ * returned by SDK functions.
+ */
+
+esp_err_t bsp_bno055_init();
+
+/** \brief Fetch a handle for the ILI9341 LCD display hardware component
+ * 
+ * \details This function returns a handle using which the ILI9341 driver can
+ *          be controlled or NULL if the ILI9341 is not available.
+ *
+ * \retval struct:ILI9341 Structure describing the ILI9341 device, used to control the driver
+ * \retval NULL           Device not available
+ */
+
+ILI9341* get_ili9341();
+
+/** \brief Fetch a handle for the RP2040 co-processor hardware component
+ * 
+ * \details This function returns a handle using which the RP2040 driver can
+ *          be controlled or NULL if the RP2040 is not available.
+ *
+ * \retval struct:ILI9341 Structure describing the RP2040 device, used to control the driver
+ * \retval NULL           Device not available
+ */
+
+RP2040* get_rp2040();
+
+/** \brief Fetch a handle for the ICE40 FPGA hardware component
+ * 
+ * \details This function returns a handle using which the ICE40 driver can
+ *          be controlled or NULL if the ICE40 is not available.
+ *
+ * \retval struct:ILI9341 Structure describing the ICE40 device, used to control the driver
+ * \retval NULL           Device not available
+ */
+
+ICE40* get_ice40();
+
+/** \brief Fetch a handle for the BNO055 sensor hardware component
+ * 
+ * \details This function returns a handle using which the BNO055 driver can
+ *          be controlled or NULL if the BNO055 is not available.
+ *
+ * \retval struct:ILI9341 Structure describing the BNO055 device, used to control the driver
+ * \retval NULL           Device not available
+ */
+
+BNO055* get_bno055();
---
+++ mch2022_badge.h
@@ -0,0 +1,49 @@
+/*
+ * MCH2022 badge
+ * 
+ * This file describes the connections from the ESP32 to the other
+ * components on the MCH2022 badge.
+ * 
+ * More information can be found in the documentation at
+ * https://www.badge.team/docs/badges/mch2022/pinout/
+ * 
+ */
+
+#pragma once
+
+#define GPIO_I2S_MCLK         0
+#define GPIO_UART_TX          1
+#define GPIO_SD_D0            2
+#define GPIO_UART_RX          3
+#define GPIO_I2S_LR           4
+#define GPIO_LED_DATA         5
+#define GPIO_I2S_CLK          12
+#define GPIO_I2S_DATA         13
+#define GPIO_SD_CLK           14
+#define GPIO_SD_CMD           15
+#define GPIO_SPI_CLK          18
+#define GPIO_SD_PWR           19 // Also LED power
+#define GPIO_I2C_SYS_SCL      21
+#define GPIO_I2C_SYS_SDA      22
+#define GPIO_SPI_MOSI         23
+#define GPIO_LCD_RESET        25
+#define GPIO_LCD_MODE         26
+#define GPIO_SPI_CS_FPGA      27
+#define GPIO_SPI_CS_LCD       32
+#define GPIO_SPI_DC_LCD       33
+#define GPIO_INT_RP2040       34 // Active low
+#define GPIO_SPI_MISO         35
+#define GPIO_INT_BNO055       36 // Active low
+#define GPIO_INT_FPGA         39 // Active low
+
+// I2C bus
+#define I2C_BUS_SYS           0
+#define I2C_SPEED_SYS         8000 // 8 kHz
+
+#define RP2040_ADDR           0x17 // RP2040 co-processor
+#define BNO055_ADDR           0x28 // BNO055 position sensor
+
+// SPI bus
+#define SPI_BUS               VSPI_HOST
+#define SPI_MAX_TRANSFER_SIZE 4094
+#define SPI_DMA_CHANNEL       2
