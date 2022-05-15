a28c60189f21cf84a945dbd16b3916ec5d353019
Renze Nicolai <renze@rnplus.nl>
2022-05-15 02:45:50 +0200

    Apply formatting

--- hardware.c
+++ hardware.c
@@ -1,12 +1,14 @@
 #include "hardware.h"
+
+#include <driver/gpio.h>
 #include <driver/spi_master.h>
 #include <esp_log.h>
-#include <driver/gpio.h>
-#include "managed_i2c.h"
-#include "sdcard.h"
-#include "rp2040.h"
 
-static const char *TAG = "hardware";
+#include "managed_i2c.h"
+#include "rp2040.h"
+#include "sdcard.h"
+
+static const char* TAG = "hardware";
 
 static BNO055  dev_bno055  = {0};
 static ILI9341 dev_ili9341 = {0};
@@ -15,13 +17,13 @@ static RP2040  dev_rp2040  = {0};
 
 static uint8_t rp2040_fw_version = 0;
 
-static bool bsp_ready = false;
+static bool bsp_ready    = false;
 static bool rp2040_ready = false;
-static bool ice40_ready = false;
+static bool ice40_ready  = false;
 static bool bno055_ready = false;
 
 esp_err_t ice40_get_done_wrapper(bool* done) {
-    uint16_t buttons;
+    uint16_t  buttons;
     esp_err_t res = rp2040_read_buttons(&dev_rp2040, &buttons);
     if (res != ESP_OK) return res;
     *done = !((buttons >> 5) & 0x01);
@@ -54,13 +56,13 @@ static esp_err_t _bus_init() {
 
     // SPI bus
     spi_bus_config_t busConfiguration = {0};
-    busConfiguration.mosi_io_num     = GPIO_SPI_MOSI;
-    busConfiguration.miso_io_num     = GPIO_SPI_MISO;
-    busConfiguration.sclk_io_num     = GPIO_SPI_CLK;
-    busConfiguration.quadwp_io_num   = -1;
-    busConfiguration.quadhd_io_num   = -1;
-    busConfiguration.max_transfer_sz = SPI_MAX_TRANSFER_SIZE;
-    res = spi_bus_initialize(SPI_BUS, &busConfiguration, SPI_DMA_CHANNEL);
+    busConfiguration.mosi_io_num      = GPIO_SPI_MOSI;
+    busConfiguration.miso_io_num      = GPIO_SPI_MISO;
+    busConfiguration.sclk_io_num      = GPIO_SPI_CLK;
+    busConfiguration.quadwp_io_num    = -1;
+    busConfiguration.quadhd_io_num    = -1;
+    busConfiguration.max_transfer_sz  = SPI_MAX_TRANSFER_SIZE;
+    res                               = spi_bus_initialize(SPI_BUS, &busConfiguration, SPI_DMA_CHANNEL);
     if (res != ESP_OK) {
         ESP_LOGE(TAG, "Initializing SPI bus failed");
         return res;
@@ -71,7 +73,7 @@ static esp_err_t _bus_init() {
 
 esp_err_t bsp_init() {
     if (bsp_ready) return ESP_OK;
-    
+
     esp_err_t res;
 
     // Interrupts
@@ -84,18 +86,18 @@ esp_err_t bsp_init() {
     // Communication busses
     res = _bus_init();
     if (res != ESP_OK) return res;
-    
+
     // LCD display
-    dev_ili9341.spi_bus   = SPI_BUS;
-    dev_ili9341.pin_cs    = GPIO_SPI_CS_LCD;
-    dev_ili9341.pin_dcx   = GPIO_SPI_DC_LCD;
-    dev_ili9341.pin_reset = GPIO_LCD_RESET;
-    dev_ili9341.rotation  = 1;
-    dev_ili9341.color_mode = true; // Blue and red channels are swapped
-    dev_ili9341.spi_speed = 60000000; // 60MHz
+    dev_ili9341.spi_bus               = SPI_BUS;
+    dev_ili9341.pin_cs                = GPIO_SPI_CS_LCD;
+    dev_ili9341.pin_dcx               = GPIO_SPI_DC_LCD;
+    dev_ili9341.pin_reset             = GPIO_LCD_RESET;
+    dev_ili9341.rotation              = 1;
+    dev_ili9341.color_mode            = true;      // Blue and red channels are swapped
+    dev_ili9341.spi_speed             = 60000000;  // 60MHz
     dev_ili9341.spi_max_transfer_size = SPI_MAX_TRANSFER_SIZE;
-    dev_ili9341.callback = ili9341_set_lcd_mode; // Callback for changing LCD mode between ESP32 and FPGA
-    
+    dev_ili9341.callback              = ili9341_set_lcd_mode;  // Callback for changing LCD mode between ESP32 and FPGA
+
     res = gpio_set_direction(GPIO_LCD_MODE, GPIO_MODE_OUTPUT);
     if (res != ESP_OK) {
         ESP_LOGE(TAG, "Initializing LCD mode GPIO failed");
@@ -107,7 +109,7 @@ esp_err_t bsp_init() {
         ESP_LOGE(TAG, "Initializing LCD failed");
         return res;
     }
-    
+
     bsp_ready = true;
     return ESP_OK;
 }
@@ -117,11 +119,11 @@ esp_err_t bsp_rp2040_init() {
     if (rp2040_ready) return ESP_OK;
 
     // RP2040 co-processor
-    dev_rp2040.i2c_bus = I2C_BUS_SYS;
-    dev_rp2040.i2c_address = RP2040_ADDR;
+    dev_rp2040.i2c_bus       = I2C_BUS_SYS;
+    dev_rp2040.i2c_address   = RP2040_ADDR;
     dev_rp2040.pin_interrupt = GPIO_INT_RP2040;
-    dev_rp2040.queue = xQueueCreate(8, sizeof(rp2040_input_message_t));
-    
+    dev_rp2040.queue         = xQueueCreate(8, sizeof(rp2040_input_message_t));
+
     esp_err_t res = rp2040_init(&dev_rp2040);
     if (res != ESP_OK) {
         ESP_LOGE(TAG, "Initializing RP2040 failed");
@@ -140,19 +142,19 @@ esp_err_t bsp_rp2040_init() {
 esp_err_t bsp_ice40_init() {
     if (!bsp_ready) return ESP_FAIL;
     if (!rp2040_ready) return ESP_FAIL;
-    if (rp2040_fw_version == 0xFF) return ESP_FAIL; // The ICE40 FPGA can only be controlled when the RP2040 is not in bootloader mode
+    if (rp2040_fw_version == 0xFF) return ESP_FAIL;  // The ICE40 FPGA can only be controlled when the RP2040 is not in bootloader mode
     if (ice40_ready) return ESP_OK;
 
-    dev_ice40.spi_bus = SPI_BUS;
-    dev_ice40.pin_cs = GPIO_SPI_CS_FPGA;
-    dev_ice40.pin_done = -1;
-    dev_ice40.pin_reset = -1;
-    dev_ice40.pin_int = GPIO_INT_FPGA;
+    dev_ice40.spi_bus               = SPI_BUS;
+    dev_ice40.pin_cs                = GPIO_SPI_CS_FPGA;
+    dev_ice40.pin_done              = -1;
+    dev_ice40.pin_reset             = -1;
+    dev_ice40.pin_int               = GPIO_INT_FPGA;
     dev_ice40.spi_speed_full_duplex = 26700000;
     dev_ice40.spi_speed_half_duplex = 40000000;
     dev_ice40.spi_max_transfer_size = SPI_MAX_TRANSFER_SIZE;
-    dev_ice40.get_done = ice40_get_done_wrapper;
-    dev_ice40.set_reset = ice40_set_reset_wrapper;
+    dev_ice40.get_done              = ice40_get_done_wrapper;
+    dev_ice40.set_reset             = ice40_set_reset_wrapper;
 
     esp_err_t res = ice40_init(&dev_ice40);
     if (res != ESP_OK) {
@@ -185,7 +187,7 @@ esp_err_t bsp_bno055_init() {
         ESP_LOGE(TAG, "Initializing BNO055 failed");
         return res;
     }
-    
+
     res = bno055_set_power_mode(&dev_bno055, BNO055_POWER_MODE_SUSPEND);
     if (res != ESP_OK) {
         ESP_LOGE(TAG, "Failed to switch BNO055 power mode to suspended state");
--- hardware.h
+++ hardware.h
@@ -1,26 +1,25 @@
 #pragma once
 
-#include <sdkconfig.h>
-#include <esp_err.h>
 #include <driver/spi_master.h>
+#include <esp_err.h>
+#include <sdkconfig.h>
 
 #include "bno055.h"
-#include "ili9341.h"
 #include "ice40.h"
+#include "ili9341.h"
+#include "pins.h"
 #include "rp2040.h"
 
-#include "pins.h"
-
 /** \brief Initialize basic board support
- * 
+ *
  * \details This function installs the GPIO ISR (interrupt service routine) service
  *          which allows for per-pin GPIO interrupt handlers. Then it initializes the
  *          I2C and SPI communication busses and the LCD display driver. Returns ESP_OK
- *          on success and a 
+ *          on success and a
  *
  * \retval ESP_OK   The function succesfully executed
  * \retval ESP_FAIL The function failed, possibly indicating hardware failure
- * 
+ *
  * Check the esp_err header file from the ESP-IDF for a complete list of error codes
  * returned by SDK functions.
  */
@@ -28,7 +27,7 @@
 esp_err_t bsp_init();
 
 /** \brief Initialize RP2040 driver and check I2C communication with the RP2040 co-processor
- * 
+ *
  * \details This function initializes the RP2040 driver, which installs the RP2040 interrupt
  *          handler task. Then it queries the version of the firmware running on the RP2040.
  *          Reading the firmware version verifies that communication via I2C is funcitonal and
@@ -37,7 +36,7 @@ esp_err_t bsp_init();
  *
  * \retval ESP_OK   The function succesfully executed
  * \retval ESP_FAIL The function failed, possibly indicating hardware failure
- * 
+ *
  * Check the esp_err header file from the ESP-IDF for a complete list of error codes
  * returned by SDK functions.
  */
@@ -45,13 +44,13 @@ esp_err_t bsp_init();
 esp_err_t bsp_rp2040_init();
 
 /** \brief Initialize the ICE40 driver and perform basic sanity checks
- * 
+ *
  * \details This function initializes the ICE40 driver and checks that the status
  *          indication (the "done" pin) correctly indicates that the ICE40 is disabled.
  *
  * \retval ESP_OK   The function succesfully executed
  * \retval ESP_FAIL The function failed, possibly indicating hardware failure
- * 
+ *
  * Check the esp_err header file from the ESP-IDF for a complete list of error codes
  * returned by SDK functions.
  */
@@ -59,13 +58,13 @@ esp_err_t bsp_rp2040_init();
 esp_err_t bsp_ice40_init();
 
 /** \brief Initialize the BNO055 driver and put the sensor into power saving mode
- * 
+ *
  * \details This function initializes the BNO055 driver and puts the sensor into
  *          power saving mode.
  *
  * \retval ESP_OK   The function succesfully executed
  * \retval ESP_FAIL The function failed, possibly indicating hardware failure
- * 
+ *
  * Check the esp_err header file from the ESP-IDF for a complete list of error codes
  * returned by SDK functions.
  */
@@ -73,7 +72,7 @@ esp_err_t bsp_ice40_init();
 esp_err_t bsp_bno055_init();
 
 /** \brief Fetch a handle for the ILI9341 LCD display hardware component
- * 
+ *
  * \details This function returns a handle using which the ILI9341 driver can
  *          be controlled or NULL if the ILI9341 is not available.
  *
@@ -84,7 +83,7 @@ esp_err_t bsp_bno055_init();
 ILI9341* get_ili9341();
 
 /** \brief Fetch a handle for the RP2040 co-processor hardware component
- * 
+ *
  * \details This function returns a handle using which the RP2040 driver can
  *          be controlled or NULL if the RP2040 is not available.
  *
@@ -95,7 +94,7 @@ ILI9341* get_ili9341();
 RP2040* get_rp2040();
 
 /** \brief Fetch a handle for the ICE40 FPGA hardware component
- * 
+ *
  * \details This function returns a handle using which the ICE40 driver can
  *          be controlled or NULL if the ICE40 is not available.
  *
@@ -106,7 +105,7 @@ RP2040* get_rp2040();
 ICE40* get_ice40();
 
 /** \brief Fetch a handle for the BNO055 sensor hardware component
- * 
+ *
  * \details This function returns a handle using which the BNO055 driver can
  *          be controlled or NULL if the BNO055 is not available.
  *
